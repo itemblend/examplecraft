a9878403629959f92d425fb916db96f8c135d50f
juan3178 <juan3178316@gmail.com>
2025-05-03 22:45:21 -0500

    Create database_items.js

---
+++ code/fire_resistence_item/behavior_pack/scripts/ibs/database_items.js
@@ -0,0 +1,8 @@
+export const __itemFireResistenceId_0x0000 = [
+	"minecraft:clock",
+	"minecraft:bread",
+	"minecraft:diamond",
+	"minecraft:diamond_block",
+	"minecraft:iron_ingot",
+	"minecraft:obsidian"
+];
